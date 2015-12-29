570566642edbb094bd801d16e69393ead550f9ba
lusciouspear <taylor69>
2015-12-29 11:06:34 +0100

    cleanup

--- src/main/java/com/acacia/angleddream/common/BigQueryProcessor.java
+++ src/main/java/com/acacia/angleddream/common/BigQueryProcessor.java
@@ -3,6 +3,8 @@ package com.acacia.angleddream.common;
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -12,15 +14,23 @@ public class BigQueryProcessor extends DoFn<String, TableRow> {
 
     static final Gson gson = new Gson();
 
+    private static final Logger LOG = LoggerFactory.getLogger(BigQueryProcessor.class);
+
+
     @Override
     public void processElement(ProcessContext c) {
 
 
         //turn input string into json object for consumption by bq
 
+//        System.out.println("BigQuery input:" + c.element());
+
+
         Map<String, Object> hm = gson.<Map<String, Object>>fromJson(
                 c.element(),
                 (new HashMap<String, Object>()).getClass());
+
+
 
         if (hm != null){
 
