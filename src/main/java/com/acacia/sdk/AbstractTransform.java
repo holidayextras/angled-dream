7aae909b9d8fd7a2a76f81105dd83735932060e3
lusciouspear <bradfordstephens@gmail.com>
2016-02-15 21:24:05 -0800

    unabstract methods??

--- src/main/java/com/acacia/sdk/AbstractTransform.java
+++ src/main/java/com/acacia/sdk/AbstractTransform.java
@@ -20,9 +20,13 @@ public abstract class AbstractTransform extends DoFn<String,String> {
     static final TupleTag<String> errorOutput = new TupleTag<String>("errorOutput"){};
 
     //function to transform a string, used by processElement
+
     public String transform(String input) throws GenericDataflowAppException {return input;}
 
+
+
     public String transform(String input, Map<String,String> args) throws GenericDataflowAppException {return input;}
+
 
     static final Gson gson = new Gson();
 
